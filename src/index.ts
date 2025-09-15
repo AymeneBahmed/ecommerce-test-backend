d15e04bfb3086f2b20688dc8e3d7f80ddce2757e
noob <joewilliamdd31@gmail.com>
2025-09-15 15:37:49 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hi there");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
+});
